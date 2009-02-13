b1d5c264a678a7a61b210e1209cc88a9a4b3fd08
dmeliza <dmeliza@uchicago.edu>
2009-02-13 15:43:46 -0600

    test.py, test.c: stuff to test fft and sonogram
    darcs-hash:20090213214346-21d25-d8c7fabd58bb21c3b31360d5de4c32b3f4ced8a2.gz

---
+++ test.c
@@ -0,0 +1,45 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <pcmio.h>
+#include "sonogram.h"
+
+int
+main(int argv, char **argc)
+{
+	short *data;
+	int nsamples;
+	PCMFILE *fp = pcm_open("A7.pcm","r");
+	int i =0;
+	float *psd;
+
+	pcm_read(fp, &data, &nsamples);
+	printf("Number of samples: %d\n", nsamples);
+
+	struct sonogram *sono = create_sonogram();
+
+	printf("Setting options\n");
+	sonogram_setopts(sono, SONO_OPT_FFTSIZE, 256);
+	sonogram_setopts(sono, SONO_OPT_OVERLAP, 50);
+	sonogram_setopts(sono, SONO_OPT_METHOD, METHOD_STFT);
+	sonogram_setopts(sono, SONO_OPT_WINDOW, WINDOW_HANNING);
+	
+	sonogram_setopts(sono, SONO_OPT_WINDOW, WINDOW_MULTITAPER);
+	sonogram_setopts(sono, SONO_OPT_MTM_NW, 3.5);
+	sonogram_setopts(sono, SONO_OPT_MTM_NTAPERS, 5);
+	sonogram_setopts(sono, SONO_OPT_MTM_ADAPT, 1);
+	printf("Set options\n");
+	
+
+	FILE *out = fopen("testsono.bin","w");
+
+	for (i = 0; i < 100; i++) {
+		psd = calculate_psd_uncached_column(sono, data, nsamples, i, 0, 0.5);
+		fwrite(psd, sizeof(float), 127, out);
+	}
+
+	pcm_close(fp);
+	fclose(out);
+	return (0);
+}
+	
--- test.py
+++ test.py
@@ -4,38 +4,64 @@ import ctypes as cx
 from numpy.ctypeslib import ndpointer
 
 lsono = cx.cdll.LoadLibrary('libsono2.dylib')
+#lsono1 = cx.cdll.LoadLibrary('libsono.dylib')
 
 lsono.dpss.restypes = None
 lsono.dpss.argtypes = [ndpointer(dtype='d'), ndpointer(dtype='d'),
                        cx.c_int, cx.c_double, cx.c_int]
 
-lsono.init_mtm_prealloc.restype = cx.c_void_p
-lsono.init_mtm_prealloc.argtypes = [cx.c_int, cx.c_int, cx.c_int,
+lsono.mtm_init.restype = cx.c_void_p
+lsono.mtm_init.argtypes = [cx.c_int, cx.c_int, cx.c_int,
                                     ndpointer(dtype='d'), ndpointer(dtype='d')]
+lsono.mtm_init_dpss.restype = cx.c_void_p
+lsono.mtm_init_dpss.argtypes = [cx.c_int, cx.c_double, cx.c_int]
 
 lsono.mtfft.restype = cx.c_double
 lsono.mtfft.argtypes = [cx.c_void_p, ndpointer(dtype='h'), cx.c_int]
 
 lsono.mtpower.argtypes = [cx.c_void_p, ndpointer(dtype='d'), cx.c_double]
 
-N = 100
-NW = 3.5
-k = 5
+lsono.create_sonogram.restype = cx.c_void_p
+lsono.sonogram_setopts.argtypes = [cx.c_void_p, cx.c_int, cx.c_long]
 
-# allocate storage
-tapers = nx.zeros(N*k)
-lambdas = nx.zeros(k)
-test = (nx.random.randn(N) * 100).astype('h')
-out = nx.zeros(N)
+lsono.getbuffer.argtypes = [cx.c_void_p, ndpointer(dtype='d')]
 
-# compute fft
-lsono.dpss(tapers, lambdas, N, NW, k)
-mtm = lsono.init_mtm_prealloc(N, N, k, tapers, lambdas)
-sigpow = lsono.mtfft(mtm, test, N)
-lsono.mtpower(mtm, out, sigpow)
+if __name__=="__main__":
+    N = 256
+    NW = 3.5
+    k = 5
 
-# test against python code
-## from dlab import signalproc
-## lambda2,tapers2 = signalproc.dpss(N,NW,k)
-## J,f = signalproc.mtfft(test,nfft=N,mtm_p=NW,tapers=k)
-## out2 = nx.absolute(J**2).mean(1)
+    # allocate storage
+    #tapers = nx.zeros(N*k)
+    #lambdas = nx.zeros(k)
+    from dlab import pcmio
+    s = pcmio.sndfile('A7.pcm').read()
+    #test = s[:N]  #(nx.random.randn(N) * 100).astype('h')
+    test = s[6800:(6800+N)]
+    out = nx.zeros(N/2+1)
+    out2 = nx.zeros_like(out)
+    out3 = nx.zeros_like(out)
+
+    # compute fft
+    #lsono.dpss(tapers, lambdas, N, NW, k)
+    #mtm = lsono.mtm_init(N, N, k, tapers, lambdas)
+    mtm = lsono.mtm_init_dpss(N, NW, k)
+    #for i in range(100):
+    sigpow = lsono.mtfft(mtm, test, N)
+    lsono.mtpower(mtm, out, sigpow)
+    lsono.mtpower(mtm, out2, 0.0)
+    #lsono.destroy_mtm(mtm)
+
+    # test with single window
+    window = nx.hanning(N)
+    ll = nx.ones(1)
+    mtm2 = lsono.mtm_init(N, N, 1, window, ll)
+    for i in range(100):
+        sigpow = lsono.mtfft(mtm2, test, N)
+        lsono.mtpower(mtm2, out3, 0.0)
+
+    # test against python code
+    from dlab import signalproc
+    lambda2,tapers2 = signalproc.dpss(N,NW,k)
+    J,f = signalproc.mtfft(test,tapers=tapers2)
+    out4 = nx.absolute(J**2).mean(1)
