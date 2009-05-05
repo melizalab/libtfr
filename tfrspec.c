11485a4c71aa266fd5bdf9e788a7c393de88d3a7
dmeliza <dmeliza@uchicago.edu>
2009-05-05 09:15:10 -0500

    tfrspec.c: working
    darcs-hash:20090505141510-21d25-6235609ab81a465c113dd00def96fa8fa2d53aa4.gz

--- tfrspec.c
+++ tfrspec.c
@@ -1,4 +1,5 @@
 #include <mex.h>
+#include <math.h>
 #include "tfr.h"
 
 /*
@@ -8,26 +9,37 @@
 void
 mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-	int m,n;
+	int m,n,nt;
 	int nfft, step, npoints;
 	int ntapers = 6;
 	int tlock = 5;
 	double tm = 6.0;
 	double flock=0.01;
+	double *spec;
 	mfft* mtmh;
+
+	mxClassID data_type;
+	short *data_short;
+	float *data_float;
+	double *data_double;
 
 	if (nlhs < 1)
 		return;
 	if ((nrhs < 4) || (nrhs > 4) || (nlhs > 1))
 		mexErrMsgTxt("SP = TFRSPEC(S, N, step, Np, K, tm, flock, tlock)");
 		
-	// parse arguments
+	/* parse arguments */
 	m = mxGetM(prhs[0]);
 	n = mxGetN(prhs[0]);
 	if ((m > 1) && (n > 1))
 		mexErrMsgTxt("Input signal must be a 1-D time series");
-	// check data type
+	nt = (m > n) ? m : n;
+	/* check data type */
+	data_type = mxGetClassID(prhs[0]);
+	if (!((data_type==mxINT16_CLASS) || (data_type==mxSINGLE_CLASS) || (data_type==mxDOUBLE_CLASS)))
+		mexErrMsgTxt("Input signal must be short int, float, or double precision");
 
+	/*data_double = mxGetPr(prhs[0]);*/
 	nfft = mxGetScalar(prhs[1]);
 	step = mxGetScalar(prhs[2]);
 	npoints = mxGetScalar(prhs[3]);
@@ -40,6 +52,27 @@ mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 	if (nrhs > 7)
 		tlock = mxGetScalar(prhs[7]);
 
+	/* allocate output array */
+	plhs[0] = mxCreateDoubleMatrix(nfft/2+1, nt/step,mxREAL);
+	spec = mxGetPr(plhs[0]);
+
+	/* generate transform object */
 	mtmh = mtm_init_herm(nfft, npoints, ntapers, tm);
+
+	/* calculate spectrogram; use the right precision fxn */
+	if (data_type==mxDOUBLE_CLASS) {
+		data_double = mxGetPr(prhs[0]);
+		tfr_spec_double(mtmh, spec, data_double, nt, -1, step, flock, tlock);
+	} 
+	else if (data_type == mxSINGLE_CLASS) {
+		data_float = (float*)mxGetPr(prhs[0]);
+		tfr_spec_float(mtmh, spec, data_float, nt, -1, step, flock, tlock);
+	}
+	else if (data_type == mxINT16_CLASS) {
+		data_short = (short*)mxGetPr(prhs[0]);
+		tfr_spec(mtmh, spec, data_short, nt, -1, step, flock, tlock);
+	}
+
+
 	mtm_destroy(mtmh);
 }
