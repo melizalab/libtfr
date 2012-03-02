8785128c90da846b94274eec2d311b7d2885f984
Dan Meliza <dmeliza@uchicago.edu>
2012-03-02 11:10:06 -0600

    fixed tgrid function; updated python docs

--- libtfr.py
+++ libtfr.py
@@ -1,9 +1,18 @@
-#!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
 """
 Interface to libtfr spectrogram library using numpy.
 
-Copyright C Daniel Meliza 2010.  Licensed for use under GNU
+Spectrograms are returned as 2D arrays with frequency indexed by row
+and time by column.  Signals are assumed to be real; therefore real
+power spectrograms with a transform size of N have N/2+1 rows,
+corresponding to frequencies from 0 to Nyquist.  Complex spectrograms
+have N rows.  The number of time points in the spectrogram is (M - W +
+1)/S, where M is the length of the signal, S is the shift (S), and the
+analysis window size is W (this may be less than or equal to N).  Only
+time points corresponding to window positions that completely overlap
+with the signal are returned.
+
+Copyright C Daniel Meliza 2010-2012.  Licensed for use under GNU
 General Public License, Version 2.  See COPYING for details.
 """
 
@@ -27,8 +36,8 @@ def tfr_spec(s, N, step, Np, K=6, tm=6.0, flock=0.01, tlock=5, fgrid=None):
     fgrid - output frequency bins: monotonically increasing
             (default linear scale with N points; Nyquist is 1.0)
 
-    returns an N/2+1 by L power spectrogram (L = length(s) / step), or if
-    fgrid is specified, fgrid.size by L
+    returns an N/2+1 by L power spectrogram, or if fgrid is specified,
+    fgrid.size by L
     """
     return _libtfr.tfr_spec(s, N, step, Np, K, tm, flock, tlock, fgrid)
 
@@ -53,15 +62,11 @@ def hermf(N, order=6, tm=6.0):
     Computes a set of orthogonal Hermite functions for use in computing
     multi-taper reassigned spectrograms
 
-    N - the number of points in the window (must be odd)
-    order - the maximum order of the set of functions (default 6)
-    tm - half-time support (default 6)
+    @param N      the number of points in the window (must be odd)
+    @param order  the maximum order of the set of functions (default 6)
+    @param tm     half-time support (default 6)
 
-    Returns: h, Dh, Th
-    h - hermite functions (MxN)
-    Dh - first derivative of h (MxN)
-    Th - time-multiple of h (MxN)
-
+    @returns  hermite functions (MxN), first derivative of h (MxN), time-multiple of h (MxN)
     """
     return _libtfr.hermf(N, order, tm)
 
@@ -70,14 +75,14 @@ if hasattr(_libtfr,'mtm_spec'):
 	"""
 	Compute multitaper spectrogram using DPSS tapers
 
-	s - input signal
-	N - number of frequency points (i.e. window size)
-	step - step size
-	NW - time-frequency product
-	k - number of tapers (default NW*2-1)
-	adapt - compute adaptive spectrogram (default True)
+	@param s  input signal
+	@param N  number of frequency points (i.e. window size)
+	@param step  step size
+	@param NW time-frequency product
+	@param k  number of tapers (default NW*2-1)
+	@param adapt - compute adaptive spectrogram (default True)
 
-	returns an N/2+1 by L power spectrogram
+	@returns N/2+1 by L power spectrogram
 	"""
 	return _libtfr.mtm_spec(s, N, step, NW, k, adapt)
 
@@ -86,12 +91,12 @@ if hasattr(_libtfr,'mtm_psd'):
 	"""
 	Compute PSD of a signal using multitaper methods
 
-	s - input signal
-	NW - time-frequency product
-	k - number of tapers (default NW*2-1)
-	adapt - compute adaptive spectrum (default True)
+	@param s  input signal
+	@param NW  time-frequency product
+	@param k  number of tapers (default NW*2-1)
+	@param adapt - compute adaptive spectrum (default True)
 
-	returns an N/2+1 real power spectrum density
+	@returns  N/2+1 1D real power spectrum density
 	"""
 	return _libtfr.mtm_psd(s, NW, k, adapt)
 
@@ -101,12 +106,12 @@ if hasattr(_libtfr,'mtfft'):
 	"""
 	Compute multitaper transform of a signal
 
-	s - input signal
-	NW - time-frequency product
-	k - number of tapers (default NW*2-1)
-        N - number of points in FFT (default s.size)
+	@param s input signal
+	@param NW time-frequency product
+	@param k number of tapers (default NW*2-1)
+        @param N number of points in FFT (default s.size)
 
-	returns an NxK array of complex numbers
+	@returns 2D complex array, dimension N x k
 	"""
 	return _libtfr.mtfft(s, NW, k, N)
 
@@ -117,16 +122,15 @@ if hasattr(_libtfr,'dpss'):
 	Computes the discrete prolate spherical sequences used in the
 	multitaper method power spectrum calculations.
 
-	npoints   the number of points in the window
-	mtm_p     the time-bandwidth product. Must be an integer or half-integer
-		  (typical choices are 2, 5/2, 3, 7/2, or 4)
-	k         If a scalar, returns the 1:k DPSS vectors
-		  If a 2-ple, returns the k[0]:k[1] DPSS vectors
-		  Default is to return all vectors
+	@param npoints   the number of points in the window
+	@param mtm_p     the time-bandwidth product. Must be an integer or half-integer
+	          	 (typical choices are 2, 5/2, 3, 7/2, or 4)
+	@param k         If a scalar, returns the 1:k DPSS vectors
+                         If a 2-ple, returns the k[0]:k[1] DPSS vectors
+		         Default is to return all vectors
 
-	returns:
-	e - 2D array of eigenvectors, shape (n,npoints)
-	v - 2D array of eigenvalues, length n = (mtm_p * 2 - 1)
+	@returns (2D array of eigenvectors, shape (n,npoints),
+                  2D array of eigenvalues, length n = (mtm_p * 2 - 1))
 	"""
 	return _libtfr.dpss(N,NW,k)
 
@@ -135,12 +139,11 @@ def log_fgrid(fmin, fmax, N, Fs=None):
     """
     Generates a logarithmic frequency grid between fmin and fmax
 
-    fmin - first frequency
-    fmax  - last frequency
-    N      - number of points
-    base   - log base
-    Fs     - set to a positive value to convert values
-             to relative frequencies
+    @param fmin  first frequency
+    @param fmax  last frequency
+    @param N     number of points
+    @param base  log base
+    @param Fs    set to a positive value to convert values to relative frequencies
     """
     from numpy import log, logspace, e
     lfmin, lfmax = log((fmin, fmax))
@@ -156,15 +159,13 @@ def fgrid(Fs,nfft,fpass):
     """
     Calculate the frequency grid associated with an fft computation
 
-    Usage: [f,findx]=fgrid(Fs,nfft,fpass)
+    @param Fs        sampling frequency associated with the data
+    @param nfft      number of points in fft
+    @param fpass     upper and lower frequencies of interest,
+                     [fmin fmax), in same units as Fs
 
-    Fs        sampling frequency associated with the data
-    nfft      number of points in fft
-    fpass     upper and lower frequencies of interest,
-              [fmin fmax), in same units as Fs
-    Outputs:
-    f         frequencies
-    findx     index of the frequencies in the full frequency grid.
+    @returns (1D array of frequencies,
+              1D array indexing frequencies in the full frequency grid)
 
     Example:
 
@@ -187,29 +188,41 @@ def fgrid(Fs,nfft,fpass):
 
     return f[findx], findx
 
-
-def tgrid(siglen, Fs, shift, winsize=None):
+def tgrid(S, Fs, shift):
     """
-    Calculate the time grid associated with an STFT
+    Calculate the time grid associated with an STFT. Note that
+    spectrograms generated by libtfr do not include frames that extend
+    beyond the edge of the signal.  These can be excluded if one knows
+    the size of the analysis taper(s), but this size may be adjusted
+    silently by the functions that generate Hermitian and DPSS tapers
+    in order to ensure that the number of points is even or odd.  This
+    function will accept either a scalar or an ndarray as its first
+    argument: in the former case the full time grid (including
+    unsupported frames) will be returned; in the latter, the time grid
+    will be truncated to match the length of the spectrogram.
 
-    @param siglen    length of signal (in samples)
+    @param S         length of signal (in samples) OR the 2D spectrogram array (freq x time)
     @param Fs        sampling frequency associated with the data
     @param shift     number of samples shifted between data frames
-    @param winsize   if not None, specfies window size, which is used
-                     to exclude frames that extend beyond the signal
 
     @returns a 1D array of frame start times
     """
-    from numpy import arange
-    if winsize is None: winsize = 1
-    maxlen = (siglen - winsize + 1)/shift
-    return arange(0, 1./Fs * (siglen - winsize + 1), 1./Fs * shift)[:maxlen]
-
+    from numpy import arange, ndarray
+    if isinstance(S, ndarray):
+        if S.ndim == 1: raise ValueError, "Input must be a scalar or a 2D numpy array"
+        return arange(0, 1./Fs * S.shape[1] * shift, 1./Fs * shift)
+    else:
+        return arange(0, 1./Fs * S, 1./Fs * shift)
 
 def dynamic_range(S, dB):
     """
     Compress a spectrogram's dynamic range by thresholding all values
     dB less than the peak of S (linear scale).
+
+    @param S    the input spectrogram or spectrum
+    @param dB   the dynamic range to rescale to
+
+    @returns copy of S after thresholding
     """
     from numpy import log10,where
     smax = S.max()
